2061a1b6de13ed192898ee1777070f5891bdc1b7
yhawsly <slyyhaw@gmail.com>
2026-07-20 02:51:30 +0000

    fix: update import paths

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...(Array.isArray(nextVitals) ? nextVitals : [nextVitals]),
+  ...(Array.isArray(nextTs) ? nextTs : [nextTs]),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     ".next/**",
